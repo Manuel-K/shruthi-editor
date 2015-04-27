6b07bd3db713d8c54535421c83a69e252f350d02
Manuel K <Manuel-K@users.noreply.github.com>
2015-04-27 18:57:55 +0200

    cleanup

--- lib_fileio.cpp
+++ lib_fileio.cpp
@@ -22,61 +22,6 @@
 #include <QTextStream>
 #include <QDebug>
 
-/*
-// ******************************************
-bool FileIO::loadFromDisk(const QString &location) {
-// ******************************************
-#ifdef DEBUGMSGS
-    qDebug() << "Patch::loadFromDisk(" << location << ")";
-#endif
-    QFile file(location);
-
-    if (!file.exists()) {
-        qDebug() << "The file does not exist.";
-        return false;
-    }
-
-    if (!file.open(QIODevice::ReadOnly)) {
-        qDebug() << "Failed to open.";
-        return false;
-    }
-
-    char tmp[195];
-    unsigned int readBytes = file.read(tmp, 195);
-    file.close();
-
-#ifdef DEBUGMSGS
-    qDebug() << "Read" << readBytes << "bytes.";
-#endif
-
-    // primitive check if patch is valid:
-    if (readBytes == 195) {
-#ifdef DEBUGMSGS
-        qDebug() << "Detected full patch sysex.";
-#endif
-        Message sysex;
-        sysex.reserve(195);
-        for (unsigned int i = 0; i < 195; i++) {
-            sysex.push_back(tmp[i]);
-        }
-        return parseSysex(&sysex);
-    } else if (readBytes == 92) {
-#ifdef DEBUGMSGS
-        qDebug() << "Detected light patch files.";
-#endif
-        unsigned char sysex[92];
-        for (unsigned int i=0; i<readBytes; i++) {
-            sysex[i] = (char) tmp[i];
-    #ifdef DEBUGMSGS
-            qDebug() << i << ":" << sysex[i];
-    #endif
-        }
-        return unpackData(sysex);
-    } else {
-        return false;
-    }
-}
-*/
 
 // ******************************************
 bool FileIO::loadFromDisk(const QString &location, std::vector<unsigned char> &data) {
